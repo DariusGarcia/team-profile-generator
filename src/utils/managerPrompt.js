4f9035cfd708b32ffbeae29e522878fd3eeecee2
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-08 21:58:28 -0800

    finished manager prompt

--- src/utils/managerPrompt.js
+++ src/utils/managerPrompt.js
@@ -1,41 +1,40 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer')
 const Manager = require('../../lib/manager')
 
-function managerPrompt() {
-  console.log('Team Profile Generator started...')
-  inquirer.prompt(questions).then((managerData) => {
-    const manager = new Manager(
-      managerData.name,
-      managerData.id,
-      managerData.email,
-      managerData.office
-    )
-  });
-}
-
 const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Enter the manager's name:",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Enter the manager's ID number (Must only include numbers):",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter the manager's email: ",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "Enter the manager's office number:",
-  },
-  
-];
+	{
+		type: 'input',
+		name: 'name',
+		message: "Enter the manager's name:",
+	},
+	{
+		type: 'input',
+		name: 'id',
+		message: "Enter the manager's ID number (Must only include numbers):",
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: "Enter the manager's email: ",
+	},
+	{
+		type: 'input',
+		name: 'officeNumber',
+		message: "Enter the manager's office number:",
+	},
+]
+function managerPrompt() {
+	console.log('Team Profile Generator started...')
+	inquirer.prompt(questions).then((managerData) => {
+		const manager = new Manager(
+			managerData.name,
+			managerData.id,
+			managerData.email,
+			managerData.office
+		)
+		return manager
+	})
+}
 
 module.exports = managerPrompt
 
